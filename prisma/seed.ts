ac60c6da476bf7afd01cc2e9296ed3a5d3b6ad51
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-05 11:07:33 +0100

    prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,6 +23,37 @@ const run = async () => {
       });
     })
   );
+
+  const salt = bcrypt.genSaltSync(10);
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+      firstName: "Scott",
+      lastName: "Moss",
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
