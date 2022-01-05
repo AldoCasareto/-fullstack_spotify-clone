7d76ead1660d57b2d366b28ae4416f3d926732c2
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-05 18:09:56 +0100

    validate route

---
+++ lib/auth.ts
@@ -0,0 +1,26 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import jwt from "jsonwebtoken";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verify(token, "hello");
+        user = await prisma.user.findUnique({ where: { id } });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+      } catch (error) {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+      return handler(req, res, user);
+    }
+    return res.status(401).json({ error: "Invalid credentials" });
+  };
+};
