f264270b475e4215e9a0c57e1519cfc56599992f
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-07 10:55:06 +0100

    fix token issue

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -34,11 +34,23 @@ const PlayList = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
